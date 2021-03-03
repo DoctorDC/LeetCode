dec883b78c5f346bd6ab15cf81e7d4c55b974354
doctor8023 <doctor8023@163.com>
2021-03-03 23:55:23 +0800

    new code real testing

---
+++ MathDemo/app/src/main/java/com/example/mathdemo/book/Code01_PosArrayToBST.java
@@ -0,0 +1,63 @@
+package com.example.mathdemo.book;
+
+public class Code01_PosArrayToBST {
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int v) {
+            value = v;
+        }
+    }
+
+    //2436875
+    public static Node posArrayToBST1(int[] posArr) {
+        // 0~N-1
+        return process1(posArr, 0, posArr.length - 1);
+    }
+
+    public static Node process1(int[] posArr, int L, int R) {
+        if (L > R) return null;
+        // L <= R
+        // [L ... R]
+        Node head = new Node(posArr[R]);
+        if (L == R)
+            return head;
+        // L < R-1
+        int M = L - 1;
+        for (int i = L; i < R; i++) {
+            if (posArr[i] < posArr[R]) {
+                M = i;
+            }
+        }
+        // [L .. M] < head   [M+1...R-1] > head
+        head.left = process1(posArr, L, M);
+        head.right = process1(posArr, M + 1, R - 1);
+        return head;
+    }
+
+
+    public static Node process3(int[] posArr, int L, int R) {
+        if (L > R) return null;
+        Node head = new Node(posArr[R]);
+        if (L == R)
+            return head;
+        int M = L -1;
+        int left = L;
+        int right = R-1;
+        while ( left <= right) {
+            int mid = left + ((right - left) >>1);
+            if (posArr[mid] < posArr[R]) {
+                M = mid;
+                left = mid +1;
+            } else {
+                right = mid -1;
+            }
+        }
+        head.left = process1(posArr, L, M);
+        head.right = process1(posArr, M + 1, R - 1);
+        return head;
+    }
+
+}
