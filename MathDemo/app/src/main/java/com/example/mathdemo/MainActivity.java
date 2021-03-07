c60e656b3bf57f1d6c77d2ab210bbb604940481e
doctor8023 <doctor8023@163.com>
2021-03-07 22:59:08 +0800

    é¾è¡¨ç¸äº¤

--- MathDemo/app/src/main/java/com/example/mathdemo/MainActivity.java
+++ MathDemo/app/src/main/java/com/example/mathdemo/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 //        String dd = "abccd";
 //        int pp = HighFreeCode.lengthOfLongestSubstring2(dd);
 //        Log.d(TAG, "onCreate: pp=="+pp);
-
+        Shuffler.test();
 
 
     }
